6870d4594be88123564f97f271cff5492603f61c
@entrepreneur123 <irusha2056@gmail.com>
2022-11-25 14:51:00 +0545

    loginjwt

---
+++ src/users/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = any;
+@Injectable()
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'change',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'dbhere',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => username === username);
+  }
+}
